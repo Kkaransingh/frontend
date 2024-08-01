6f13c8628a74b402dce3b2aa9592dce4bde856db
GhosTTBoy <karankamal1807@gmail.com>
2024-08-01 12:26:10 +0530

    1

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,8 +3,8 @@ import logo from '../assest/logo.png'; // Import the logo image
 
 const Logo = () => {
   return (
-    <div className="logo-container">
-      <img src={logo} alt="Logo" className="logo" />
+    <div className="logo-container flex justify-center items-center">
+      <img src={logo} alt="Logo" className="logo w-48 h-auto" />
     </div>
   );
 };
